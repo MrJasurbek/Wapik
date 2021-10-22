01af812b528929c0f470f3a9f0d8abf7830d489c
MrJasur <88433337+MrJasurbek@users.noreply.github.com>
2021-10-22 17:42:57 +0500

    tugadi Co-authored-by: uzregxurshid <uzregxurshid@users.noreply.github.com>

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let hamburger = document.querySelector(".toggle")
+// console.log(hamburger);
+let menu =  document.querySelector(".menu")
+let img = document.querySelector(".gam")
+
+// console.log(menu);
+hamburger.addEventListener("click", ()=>{
+    menu.classList.toggle("dflex")
+    if (menu.classList.contains("dflex")){
+        img.src="./img/close_black_24dp.svg"
+    } else 
+    {
+        img.src="./img/menu_black_24dp.svg"
+    }
+})
